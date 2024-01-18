cb4a5ab85760cfe8c4bff90e4c4a5e5e0c439bad
tom <aa123593465@gmail.com>
2024-01-18 21:11:03 +0800

    fix: skill layout

--- src/components/SideProject.tsx
+++ src/components/SideProject.tsx
@@ -2,17 +2,22 @@ import Link from "next/link";
 
 export default function SideProject() {
   return (
-    <div className="grid grid-cols-3 gap-4 ">
-      <Link
-        className=" border-2 border-gray-400 shadow-black p-4 rounded-md shadow-md font-bold hover:scale-105  transition-all  "
-        href="https://github.com/Tomlord1122/Traveler">
-        Traveler
-      </Link>
-      <Link
-        className=" border-2 border-gray-400 shadow-black p-4 rounded-md shadow-md font-bold hover:scale-105  transition-all  "
-        href="https://github.com/Tomlord1122/Tomlord1122.github.io">
-        Portfolio
-      </Link>
-    </div>
+    <main className="flex flex-col w-full">
+      <h1 className=" text-4xl font-bold bg-gradient-to-r from-slate-600 via-slate-800 to-black   text-stone-300">
+        Side Project
+      </h1>
+      <div className="grid grid-cols-3 gap-4 ">
+        <Link
+          className=" border-2 border-gray-400 shadow-black p-4 rounded-md shadow-md font-bold hover:scale-105  transition-all hover:bg-blue-400  "
+          href="https://github.com/Tomlord1122/Traveler">
+          Traveler
+        </Link>
+        <Link
+          className=" border-2 border-gray-400 shadow-black p-4 rounded-md shadow-md font-bold hover:scale-105  transition-all  hover:bg-blue-400 "
+          href="https://github.com/Tomlord1122/Tomlord1122.github.io">
+          Portfolio
+        </Link>
+      </div>
+    </main>
   );
 }
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -2,7 +2,7 @@ import { BentoGrid } from "./BentoGrid";
 
 export default function Skill() {
   return (
-    <main className="flex flex-col md:flex md:flex-row">
+    <main className="flex flex-col-reverse md:flex md:flex-row">
       <div className="w-full md:w-1/2 h-screen grid grid-cols-3 grid-rows-3 gap-2 p-2">
         <BentoGrid
           className="row-span-1 col-span-1   hover:scale-110 transition-all  hover:bg-blue-400"
@@ -26,8 +26,8 @@ export default function Skill() {
           height={200}></BentoGrid>
 
         <BentoGrid
-          className="row-span-1 col-span-1 mt-20 hover:scale-110 transition-all  hover:bg-blue-400"
-          img="/next.svg"
+          className="row-span-1 col-span-1  hover:scale-110 transition-all  hover:bg-blue-400"
+          img="/next-js.svg"
           width={200}
           height={100}></BentoGrid>
         <BentoGrid
@@ -47,7 +47,14 @@ export default function Skill() {
           height={100}></BentoGrid>
       </div>
 
-      <p> </p>
+      {/* Side Project Part */}
+      {/* <div className="md:w-1/2 w-full mt-5  flex flex-col justify-center items-center ">
+        {" "}
+        <section>
+          <h1 className=" text-4xl">asdasdsadsadasdasd</h1>
+          <p>asdasdasddasadss</p>
+        </section>
+      </div> */}
     </main>
   );
 }
