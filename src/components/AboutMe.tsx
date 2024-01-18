3251e11e00e1f394a1d3d18b887010216ae441ed
tom <aa123593465@gmail.com>
2024-01-18 15:21:58 +0800

    fix: apos issue

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,10 +4,10 @@ export default function AboutMe() {
     <div className=" flex justify-between h-screen">
       <div className="flex flex-col justify-center items-center ml-10">
         <div className=" text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black">
-          Hi, I'm Tom
+          Hi, I&apos;m Tom
         </div>
         <div className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black animate-wiggle">
-          I'm a software engineer
+          I&apos;<menu type="toolbar"></menu> a software engineer
         </div>
         <div className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black animate-wiggle ">
           Study at NTU
