8216d237abf0cba1d1fe88fbb19883f6e3a862a8
tomlord <aa123593465@gmail.com>
2024-09-03 13:37:58 +0800

    fix: image size in different device

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,28 +1,30 @@
 import Image from "next/image";
 export default function AboutMe() {
   return (
-    <div className=" justify-evenly h-screen  md:flex ">
+    <div className="justify-evenly h-screen md:flex">
       <div className="flex flex-col justify-center items-center ml-10">
-        <div className=" text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black">
+        <div className="text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black">
           Hi, I&apos;m Tom
         </div>
         <div className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black animate-wiggle">
           I&apos;m a software engineer
         </div>
-        <div className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black animate-wiggle ">
+        <div className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black animate-wiggle">
           Study at NTU
         </div>
-        <div className=" text-base font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black animate-wiggle ">
+        <div className="text-base font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-600 via-slate-800 to-black animate-wiggle">
           CSIE @ National Taiwan University
         </div>
       </div>
-      <Image
-        className="  rounded-full border-gray-200  shadow-md shadow-black  border-2"
-        src="/Profile.jpg"
-        alt="Avatar"
-        layout="fixed"
-        width={1050}
-        height={720}></Image>
+      {/* max-w-[300px] md:max-w-[400px] lg:max-w-[500px] */}
+      <div className="w-full md:max-w-[70%] aspect-square relative">
+        <Image
+          className="rounded-[20%] border-gray-200 shadow-md shadow-black border-2 object-cover"
+          src="/Profile.jpg"
+          alt="Avatar"
+          layout="fill"
+        />
+      </div>
 
       {/* Write a introduction */}
     </div>
